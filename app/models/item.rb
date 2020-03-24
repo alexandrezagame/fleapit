a44945c7fe1aa7d5561ea264fc67f05b8e613911
Felix Rönn <felix.ronn@gmail.com>
2020-03-24 16:45:11 +0100

    models in item, validations, items form, index fix, home linked pages, updated schema and seeds

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
-  # has_many_attached :pictures
+  has_many_attached :pictures
 
   validates :name, presence: true
+  validates :brand, presence: true
   validates :description, presence: true
   # validates :description, presence: true
   # validates :gender, presence: true, inclusion {in: CATEGORIES/GENDER}
