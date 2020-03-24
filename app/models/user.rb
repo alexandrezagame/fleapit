1d85386da17a852ae0a140bdab06d2c74853dc0d
Alexandre Zagame <zagamealexandre@gmail.com>
2020-03-24 14:29:02 +0100

    added avatar to the form and cloudinary gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :username, uniqueness: true
   validates :username, presence: true
+  has_one_attached :avatar
 end
