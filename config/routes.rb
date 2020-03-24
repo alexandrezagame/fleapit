ef57e741943ba88de17b9fc580102f26bc5a1987
Felix Rönn <felix.ronn@gmail.com>
2020-03-24 18:50:19 +0100

    destroyfunction

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :items, only: [:index, :create, :new]
+  resources :items, only: [:index, :create, :new, :destroy]
 end
