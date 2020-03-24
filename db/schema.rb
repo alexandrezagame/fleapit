3f3a7da1ee894e52954ef89f682f08dfa0cd0ac2
Alexandre Zagame <zagamealexandre@gmail.com>
2020-03-24 13:35:07 +0100

    added username to the db table Users - display the sign up/in form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_153943) do
+ActiveRecord::Schema.define(version: 2020_03_24_123115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_03_23_153943) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
