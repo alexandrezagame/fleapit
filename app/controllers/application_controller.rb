556715efaedc896647b28546f134617864ed762d
Alexandre Zagame <zagamealexandre@gmail.com>
2020-03-24 15:12:19 +0100

    new migration for the avatar on the log up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
-  # include Pundit
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  protected
 
-
-  # private
-
-  # def skip_pundit?
-  #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    # devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
